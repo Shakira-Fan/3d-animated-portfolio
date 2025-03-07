075b9d358f1404eb2e413562aea890267fe575ab
范綉莉 <shakira.fang@secom.com.tw>
2025-03-07 17:30:51 +0800

    portfolio做一半

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,101 @@
-import './portfolio.css';
+import { useEffect, useRef, useState } from "react";
+import "./portfolio.css";
+import { useScroll, useTransform } from "motion/react";
+
+const items = [
+  {
+    id: 1,
+    img: "/p1.jpg",
+    title: "Full Stack Blog Application",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 2,
+    img: "/p2.jpg",
+    title: "School Management System",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 3,
+    img: "/p3.jpg",
+    title: "Real-time Chat Application",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 4,
+    img: "/p4.jpg",
+    title: "Social Media Project",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 5,
+    img: "/p5.jpg",
+    title: "Animated Portfolio Website",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+];
+
+const ListItem = ({ item }) => {
+  return (
+    <div className="pItem">
+      <div className="pImg">
+        <img src={item.img} alt="" />
+      </div>
+      <div className="pText">
+        <h1>{item.title}</h1>
+        <p>{item.desc}</p>
+        <a href={item.link}>
+          <button>View Project</button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 const Portfolio = () => {
-  return (
-    <div className=''>Portfolio</div>
-  )
-}
+  const [containerDistance, setContainerDistance] = useState(0);
+  const ref = useRef();
 
-export default Portfolio
+  useEffect(() => {
+    if (ref.current) {
+      const rect = ref.current.getBoundingClientRect();
+      setContainerDistance(rect.left);
+    }
+  }, []);
+
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  const xTranslate = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, -window.innerWidth * items.length]
+  );
+
+  return (
+    <div className="portfolio">
+      <div className="pList" style={{ x: xTranslate }}>
+        <div
+          className="empty"
+          style={{
+            width: window.innerWidth - containerDistance,
+            backgroundColor: "pink",
+          }}
+        />
+        {items.map((item) => (
+          <ListItem item={item} key={item.id} />
+        ))}
+      </div>
+      <section />
+      <section />
+      <section />
+      <section />
+    </div>
+  );
+};
+
+export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,42 @@
-import ComputerModelContainer from './computer/ComputerModelContainer';
-import LighterModelContainer from './lighter/LIghterModelContainer';
-import './services.css';
-import Counter from './Counter';
-import SkateboardModelContainer from './skateboard/SkateboardModelContainer';
-import SkatieModelContainer from './skatie/SkatieModelContainer';
+import ComputerModelContainer from "./computer/ComputerModelContainer";
+import LighterModelContainer from "./lighter/LIghterModelContainer";
+import "./services.css";
+import Counter from "./Counter";
+import SkatieModelContainer from "./skatie/SkatieModelContainer";
+import { useRef, useState } from "react";
+import { motion, useInView } from "motion/react";
+
+const textVariants = {
+  initial: {
+    x: -100,
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const listVariants = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
 const services = [
   {
     id: 1,
@@ -24,37 +57,59 @@ const services = [
     counter: 46,
   },
 ];
+
 const Services = () => {
+  const [currentServiceId, setCurrentServiceId] = useState(1);
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: "-200px" });
   return (
-    <div className='services'>
+    <div className="services" ref={ref}>
       <div className="sSection left">
-        <h1 className="sTitle">How do I help?</h1>
-        <div className="seviceList">
-          {services.map((service)=>(
-            <div className="service" key={service.id}>
+        <motion.h1
+          variants={textVariants}
+          animate={isInView ? "animate" : "initial"}
+          className="sTitle"
+        >
+          How do I help?
+        </motion.h1>
+        <motion.div
+          variants={listVariants}
+          animate={isInView ? "animate" : "initial"}
+          className="serviceList"
+        >
+          {services.map((service) => (
+            <motion.div
+              variants={listVariants}
+              className="service"
+              key={service.id}
+              onClick={() => setCurrentServiceId(service.id)}
+            >
               <div className="serviceIcon">
                 <img src={service.img} alt="" />
               </div>
               <div className="serviceInfo">
                 <h2>{service.title}</h2>
-                <h3>{service.counter}Projects</h3>
+                <h3>{service.counter} Projects</h3>
               </div>
-            </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
         <div className="counterList">
-          <Counter from={0} to={104} text="Projects Completed"/>
-          <Counter from={0} to={72} text="Happy Clients"/>
+          <Counter from={0} to={104} text="Projects Completed" />
+          <Counter from={0} to={72} text="Happy Clients" />
         </div>
       </div>
       <div className="sSection right">
-        {/* <ComputerModelContainer/> */}
-        {/* <LighterModelContainer/> */}
-        {/* <SkateboardModelContainer/> */}
-        <SkatieModelContainer/>
+        {currentServiceId === 1 ? (
+          <ComputerModelContainer />
+        ) : currentServiceId === 2 ? (
+          <SkatieModelContainer />
+        ) : (
+          <LighterModelContainer />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
