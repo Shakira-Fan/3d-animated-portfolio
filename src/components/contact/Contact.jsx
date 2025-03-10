22f55f5225399c9a8cb2eedb426b166378ddb18f
范綉莉 <shakira.fang@secom.com.tw>
2025-03-10 17:28:10 +0800

    profolio完成

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,29 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <div className='contact'>Contact</div>
+    <div className='contact'>
+      <div className="cSection">
+        <form action="">
+          <h1 className="cTitle">Let&apos;s keep in touch</h1>
+          <div className="formItem">
+            <label htmlFor="">Name</label>
+            <input type="text" placeholder='John Doe' />
+          </div>
+          <div className="formItem">
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder='john@gmail.com' />
+          </div>
+          <div className="formItem">
+            <label htmlFor="">Message</label>
+           <textarea rows={10} placeholder='Write your message...'/>
+          </div>
+          <button className='formButton'>Send</button>
+        </form>
+      </div>
+      <div className="cSection">
+        SVG
+      </div>
+    </div>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./portfolio.css";
-import { useScroll, useTransform } from "motion/react";
+import { motion, useInView, useScroll, useTransform } from "motion/react";
 
 const items = [
   {
@@ -40,26 +40,73 @@ const items = [
   },
 ];
 
+const imgVariants = {
+  initial: {
+    x: -500,
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const textVariants = {
+  initial: {
+    x: 500,
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+      staggerChildren: 0.05,
+    },
+  },
+};
+
 const ListItem = ({ item }) => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
-    <div className="pItem">
-      <div className="pImg">
+    <div className="pItem" ref={ref}>
+      <motion.div
+        variants={imgVariants}
+        animate={isInView ? "animate" : "initial"}
+        className="pImg"
+      >
         <img src={item.img} alt="" />
-      </div>
-      <div className="pText">
-        <h1>{item.title}</h1>
-        <p>{item.desc}</p>
-        <a href={item.link}>
+      </motion.div>
+      <motion.div
+        variants={textVariants}
+        animate={isInView ? "animate" : "initial"}
+        className="pText"
+      >
+        <motion.h1 variants={textVariants}>{item.title}</motion.h1>
+        <motion.p variants={textVariants}>{item.desc}</motion.p>
+        <motion.a variants={textVariants} href={item.link}>
           <button>View Project</button>
-        </a>
-      </div>
+        </motion.a>
+      </motion.div>
     </div>
   );
 };
 
 const Portfolio = () => {
   const [containerDistance, setContainerDistance] = useState(0);
-  const ref = useRef();
+  const ref = useRef(null);
 
   useEffect(() => {
     if (ref.current) {
@@ -77,23 +124,45 @@ const Portfolio = () => {
   );
 
   return (
-    <div className="portfolio">
-      <div className="pList" style={{ x: xTranslate }}>
+    <div className="portfolio" ref={ref}>
+      <motion.div className="pList" style={{ x: xTranslate }}>
         <div
           className="empty"
           style={{
             width: window.innerWidth - containerDistance,
-            backgroundColor: "pink",
           }}
         />
         {items.map((item) => (
           <ListItem item={item} key={item.id} />
         ))}
+      </motion.div>
+      <section />
+      <section />
+      <section />
+      <section />
+      <section />
+      <div className="pProgress">
+        <svg width="100%" height="100%" viewBox="0 0 160 160">
+          <circle
+            cx="80"
+            cy="80"
+            r="70"
+            fill="none"
+            stroke="#ddd"
+            strokeWidth={20}
+          />
+          <motion.circle
+            cx="80"
+            cy="80"
+            r="70"
+            fill="none"
+            stroke="#dd4c62"
+            strokeWidth={20}
+            style={{ pathLength: scrollYProgress }}
+            transform="rotate(-90 80 80)"
+          />
+        </svg>
       </div>
-      <section />
-      <section />
-      <section />
-      <section />
     </div>
   );
 };
